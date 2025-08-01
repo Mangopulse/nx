dd61e98236d196af00ce962d558f2a89687e0b3b
Mohamad fahs <109221274+mohammad-fahs@users.noreply.github.com>
2025-08-01 15:37:09 -0700

    deployment requirments

--- ui/src/lib/Auth.ts
+++ ui/src/lib/Auth.ts
@@ -7,7 +7,7 @@ import { getCookie, setCookie } from "cookies-next";
 
 
 class Auth {
-    public apiUrl: string | undefined = process.env.NEXT_PUBLIC_API || "https://api-appx.cognativex.com";
+    public apiUrl: string | undefined = process.env.NEXT_PUBLIC_API || "http://13.40.220.91:8080";
     private user: User | undefined;
 
     constructor() {
